3308e4bf51317f6c633995518469a5dce704ceaf
Connor Reese <connor.reese13@gmail.com>
2020-03-05 17:12:36 +0800

    added music

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,53 +1,55 @@
-import React from 'react'
-import { Howl } from 'howler'
+import React from "react";
+import { Howl } from "howler";
 
 class Song extends React.Component {
-	state = {
-		playing: false,
-		audio: {}
-	}
-	componentDidMount() {
-		let audio = new Howl({
-			html5: true,
-			src: []
-		})
-		this.setState({ audio })
-	}
-	play = () => {
-		//
-		this.setState({
-			playing: true
-		})
-	}
-	stop = () => {
-		//
-		this.setState({
-			playing: false
-		})
-	}
-	render() {
-		return (
-			<tr className={this.state.playing ? 'playing' : ''}>
-				<td>
-					{!this.state.playing ? (
-						<i
-							className="button far fa-play-circle"
-							onClick={e => this.play()}
-						></i>
-					) : (
-						<i
-							className="button far fa-stop-circle"
-							onClick={e => this.stop()}
-						></i>
-					)}
-				</td>
-				<td>{this.props.song.name}</td>
-				<td>{this.props.song.artist}</td>
-				<td>{this.props.song.album}</td>
-				<td>{this.props.song.genre}</td>
-			</tr>
-		)
-	}
+  state = {
+    playing: false,
+    audio: {}
+  };
+  componentDidMount() {
+    let audio = new Howl({
+      html5: true,
+      src: [this.props.song.audio]
+    });
+    console.log(audio);
+    this.setState({ audio });
+  }
+  play = e => {
+    console.log(this.state.audio);
+    this.state.audio.play();
+    this.setState({
+      playing: true
+    });
+  };
+  stop = e => {
+    this.state.audio.stop();
+    this.setState({
+      playing: false
+    });
+  };
+  render() {
+    return (
+      <tr className={this.state.playing ? "playing" : ""}>
+        <td>
+          {!this.state.playing ? (
+            <i
+              className="button far fa-play-circle"
+              onClick={e => this.play()}
+            ></i>
+          ) : (
+            <i
+              className="button far fa-stop-circle"
+              onClick={e => this.stop()}
+            ></i>
+          )}
+        </td>
+        <td>{this.props.song.name}</td>
+        <td>{this.props.song.artist}</td>
+        <td>{this.props.song.album}</td>
+        <td>{this.props.song.genre}</td>
+      </tr>
+    );
+  }
 }
 
-export default Song
+export default Song;
