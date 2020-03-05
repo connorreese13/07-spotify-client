63e486d6b04618d790b2cfe917475f6c8590826f
Connor Reese <connor.reese13@gmail.com>
2020-03-05 16:14:15 +0800

    added songs table

--- src/pages/Songs.js
+++ src/pages/Songs.js
@@ -1,37 +1,39 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
-import '../styles/songs.css'
+import "../styles/songs.css";
 
-import Sidebar from '../components/Sidebar'
-import Song from '../components/Song'
+import Sidebar from "../components/Sidebar";
+import Song from "../components/Song";
 
 class Songs extends React.Component {
-	state = {
-		songs: []
-	}
-	componentWillMount() {
-		axios
-			.get(``)
-			.then(res => {
-				this.setState({})
-			})
-			.catch(err => {
-				console.log({ err })
-			})
-	}
-	render() {
-		return (
-			<div id="page">
-				<Sidebar page="songs" />
-				<div id="songs">
-					<table>
-						{/* songs */}
-					</table>
-				</div>
-			</div>
-		)
-	}
+  state = {
+    songs: []
+  };
+  componentWillMount() {
+    axios
+      .get(`${process.env.REACT_APP_API}/songs`)
+      .then(res => {
+        this.setState({ songs: res.data });
+      })
+      .catch(err => {
+        console.log({ err });
+      });
+  }
+  render() {
+    return (
+      <div id="page">
+        <Sidebar page="songs" />
+        <div id="songs">
+          <table>
+            {this.state.songs.map(item => {
+              return <Song song={item} />;
+            })}
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Songs
+export default Songs;
