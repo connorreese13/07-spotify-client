8347384e833cdc9fb294930cfe3e1da1442e3211
Connor Reese <connor.reese13@gmail.com>
2020-03-09 11:36:39 +0800

    added stripe

--- src/components/Paywall.js
+++ src/components/Paywall.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import {} from 'react-stripe-elements'
+import React from "react";
+import { Elements, StripeProvider } from "react-stripe-elements";
 
-import '../styles/paywall.css'
+import "../styles/paywall.css";
 
-import Stripe from './Stripe'
+import Stripe from "./Stripe";
 
 class Paywall extends React.Component {
-	render() {
-		return (
-			<div id="paywall" className={this.props.paywallOpen ? 'open' : ''}>
-				<div id="card">
-					<i
-						id="close"
-						className="fas fa-fw fa-times"
-						onClick={() => this.props.closePaywall()}
-					></i>
-					<h2>Upgrade to Premium</h2>
-					{/* stripe */}
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div id="paywall" className={this.props.paywallOpen ? "open" : ""}>
+        <div id="card">
+          <i
+            id="close"
+            className="fas fa-fw fa-times"
+            onClick={() => this.props.closePaywall()}
+          ></i>
+          <h2>Upgrade to Premium</h2>
+          <StripeProvider apiKey={`${process.env.REACT_APP_STRIPE_PK}`}>
+            <Elements>
+              <Stripe />
+            </Elements>
+          </StripeProvider>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Paywall
+export default Paywall;
--- src/components/Stripe.js
+++ src/components/Stripe.js
@@ -1,41 +1,68 @@
-import React from 'react'
-import { CardElement, injectStripe } from 'react-stripe-elements'
-import axios from 'axios'
+import React from "react";
+import { CardElement, injectStripe } from "react-stripe-elements";
+import axios from "axios";
 
 class Stripe extends React.Component {
-	state = {
-		message: {
-			content: '',
-			type: ''
-		}
-	}
-	pay = () => {}
-	getMessageClass = () => {
-		if (!this.state.message.type) {
-			return ''
-		} else if (this.state.message.type === 'success') {
-			return 'success'
-		} else {
-			return 'error'
-		}
-	}
-	render() {
-		return (
-			<>
-				<CardElement />
-				{this.state.message ? (
-					<div className={this.getMessageClass()}>
-						{this.state.message.content}
-					</div>
-				) : (
-					''
-				)}
-				<button className="submit" onClick={this.pay}>
-					Pay
-				</button>
-			</>
-		)
-	}
+  state = {
+    message: {
+      content: "",
+      type: ""
+    }
+  };
+  pay = () => {
+    this.props.stripe.createToken({}).then(token => {
+      axios
+        .post(`${process.env.REACT_APP_API}/pay`, token)
+        .then(res => {
+          if (res.data.status === "succeeded") {
+            this.setState({
+              message: {
+                content: "Payment Successful. Thank you.",
+                type: "success"
+              }
+            });
+          } else {
+            console.log(res.data);
+            this.setState({
+              message: {
+                content: res.data.code,
+                type: "error"
+              }
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+  };
+
+  getMessageClass = () => {
+    if (!this.state.message.type) {
+      return "";
+    } else if (this.state.message.type === "success") {
+      return "success";
+    } else {
+      return "error";
+    }
+  };
+  render() {
+    return (
+      <>
+        <CardElement />
+        {this.state.message ? (
+          <div className={this.getMessageClass()}>
+            {this.state.message.content}
+          </div>
+        ) : (
+          ""
+        )}
+        <button className="submit" onClick={this.pay}>
+          Pay
+        </button>
+      </>
+    );
+  }
 }
 
-export default injectStripe(Stripe)
+export default injectStripe(Stripe);
