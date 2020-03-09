700d7d50985b47ecdd2e50bbabbadea55c5ce04e
Connor Reese <connor.reese13@gmail.com>
2020-03-09 15:19:52 +0800

    added genres

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import '../styles/album.css'
+import "../styles/album.css";
 
 class Genre extends React.Component {
-	render() {
-		return (
-			<Link to={''} className="album">
-				<div
-					className="cover"
-					style={{ backgroundImage: `url('${this.props.genre.cover}')` }}
-				></div>
-				<h3>{this.props.genre.name}</h3>
-			</Link>
-		)
-	}
+  render() {
+    return (
+      <Link to={`genres/${this.props.genre.id}`} className="album">
+        <div
+          className="cover"
+          style={{ backgroundImage: `url('${this.props.genre.cover}')` }}
+        ></div>
+        <h3>{this.props.genre.name}</h3>
+      </Link>
+    );
+  }
 }
 
-export default Genre
+export default Genre;
--- src/pages/Genres.js
+++ src/pages/Genres.js
@@ -1,39 +1,39 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
-import '../styles/albums.css'
+import "../styles/albums.css";
 
-import Sidebar from '../components/Sidebar'
-import Genre from '../components/Genre'
+import Sidebar from "../components/Sidebar";
+import Genre from "../components/Genre";
 
 class Genres extends React.Component {
-	state = {
-		genres: []
-	}
-	componentWillMount() {
-		axios
-			.get('')
-			.then(res => {
-				this.setState({
-					genres: res.data
-				})
-			})
-			.catch(err => {
-				console.log({ err })
-			})
-	}
-	render() {
-		return (
-			<div id="page">
-				<Sidebar page="genres" />
-				<div id="albums">
-					{this.state.genres.map(genre => {
-						return <Genre genre={genre} key={genre.id} />
-					})}
-				</div>
-			</div>
-		)
-	}
+  state = {
+    genres: []
+  };
+  componentWillMount() {
+    axios
+      .get(`${process.env.REACT_APP_API}/genres`)
+      .then(res => {
+        this.setState({
+          genres: res.data
+        });
+      })
+      .catch(err => {
+        console.log({ err });
+      });
+  }
+  render() {
+    return (
+      <div id="page">
+        <Sidebar page="genres" />
+        <div id="albums">
+          {this.state.genres.map(genre => {
+            return <Genre genre={genre} key={genre.id} />;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Genres
+export default Genres;
